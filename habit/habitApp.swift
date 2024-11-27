ea0179ba3bb69fae6aaf6eb0b263601502ca9fc8
yemre <keseliemrey@gmail.com>
2024-11-27 14:17:14 +0300

    Initial Commit

---
+++ habit/habitApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitApp.swift
+//  habit
+//
+//  Created by yemre on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct habitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
